4c65abc8c6d93f5b4c607462cdfbaff0ed691b59
skycodee <brankostanisic@yahoo.com>
2021-06-29 19:18:40 +0200

    Intro to Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { QuotesListComponent } from './components/quotes-list/quotes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesListComponent
   ],
   imports: [
     BrowserModule,
